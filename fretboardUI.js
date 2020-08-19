369ce2fe50653b90a001a05c93c3587358fc3b73
ouijohn <johnsspaces@gmail.com>
2020-08-19 02:23:20 +0100

    classes built &  css organised

--- fretboardUI.js
+++ fretboardUI.js
@@ -1,5 +1,10 @@
+const messageBubble = document.querySelector(".message");
+const strings = document.querySelectorAll(".string");
+
+
 class Fret{
-    constructor(frets, speachBubble, scoreCard, startButtons, modal)
+    constructor(strings, frets, speachBubble, scoreCard, startButtons, modal)
+    strings = this.strings;
     frets = this.frets;
     speechBubble = this.speechBubble;
     scoreCard = this.scoreCard;
