d65fab2cff3136eae8bde99212c54d4ce55d5ca2
ouijohn <johnsspaces@gmail.com>
2020-08-20 04:21:02 +0100

    classes built, events started

--- fretboardUI.js
+++ fretboardUI.js
@@ -1,13 +1,73 @@
 const messageBubble = document.querySelector(".message");
 const strings = document.querySelectorAll(".string");
+const frets = document.querySelectorAll('.fret');
+const modal = document.querySelector(".modal");
+const close = document.querySelector(".close");
+const onOffButtonGroup = document.querySelectorAll(".onOffBtns");
+const onOffButtons = onOffButtonGroup[0].children[0].children;
+const onOffBtns = Array.prototype.slice.call( onOffButtons );
+
+
+
+onOffBtns.forEach((element, index, arr)=> {
+    element.addEventListener('click', ()=>
+    {console.log(arr[index]);
+    })
+})
+
+close.addEventListener('click', ()=>{
+    console.log('modal closed');
+})
+
+
+
+
+
+
+//part of a method
+// let correctFretsHide = document.querySelectorAll(a);
+// let correctFretsShow = document.querySelectorAll(a);
+// let celebration = document.querySelectorAll(a)
+
 
 
 class Fret{
-    constructor(strings, frets, speachBubble, scoreCard, startButtons, modal)
-    strings = this.strings;
-    frets = this.frets;
-    speechBubble = this.speechBubble;
-    scoreCard = this.scoreCard;
-    startButtons = this.startButtons;
-    modal = this.modal;
+    constructor(strings, frets, noteNames, speechBubble, scoreCard, startButtons, modal){
+        strings = this.strings;
+        frets = this.frets;
+        noteNames = this.noteNames;
+        speechBubble = this.speechBubble;
+        scoreCard = this.scoreCard;
+        startButtons = this.startButtons;
+        modal = this.modal;
+    }
+    notify(noteLabels, noteNames, noteColors, noteLetters){
+        
+
+    //THESE WILL BE ADDED WHEN WE CREATE A 'NEW' OBJECT
+        const GStringStart = 0;
+        const CStringStart = 12;
+        const EStringsStart = 25;
+        const AStringStart = 36;
+
+        // const newString = lastString + 12
+
+    //METHOD SUSSES O--- added in the 'new' object--- as will string number!
+        const G = ukulele.noteNames.indexOf('g');;
+        const C = ukulele.noteNames.indexOf('c');
+        const E = ukulele.noteNames.indexOf('e');
+        const A = ukulele.noteNames.indexOf('a');
+
+
+        this.noteNames = ["a", "aSharp", "b", "c", "cSharp", "d", "dSharp", "e", "f", "fSharp", "g", "gsharp", "a", "aSharp", "b", "c", "cSharp", "d", "dSharp", "e", "f", "fSharp", "g", "gsharp"];
+    //variables i and len MIGHT need to be this'd or maybe just be paramatered in the method!?
+        
+    //SET I SET I SET I SET I 
+        for (i = 4; i < 16; i++) {
+            console.log(ukulele.noteNames[i]);
+          }
+    }
 }
+
+//number of frets, start note and number of notes! 
+const fretUI = new Fret();
--- gameBits.js
+++ gameBits.js
@@ -1,3 +1,10 @@
 class GameBits{
-    constructor(fretcounter, rightWrong, scoreCounter, bubbleMsg)
+    constructor(fretcounter, rightWrong, scoreCounter, bubbleMsg, notes){
+        fretcounter = this.fretcounter;
+        notes = this.notes;
+        this.noteNames = ["a", "aSharp", "b", "c", "cSharp", "d", "dSharp", "e", "f", "fSharp", "g", "gsharp", "a", "aSharp", "b", "c", "cSharp", "d", "dSharp", "e", "f", "fSharp", "g", "gsharp"];
+
+    }
+        
 }
+const ukulele = new GameBits();
