5a11c753d55c9ff506eac90b311d7d65deb49c52
ouijohn <johnsspaces@gmail.com>
2020-08-30 22:38:16 +0100

    mouse events done

--- fretboardUI.js
+++ fretboardUI.js
@@ -1,94 +1,28 @@
 const messageBubble = document.querySelector(".message");
 const strings = document.querySelectorAll(".string");
 const frets = document.querySelectorAll('.fret');
+const fretNotes = document.querySelectorAll('.fretNoteLetter')
 const modal = document.querySelector(".modal");
 const close = document.querySelector(".close");
 const onOffButtonGroup = document.querySelectorAll(".onOffBtns");
 const onOffButtons = onOffButtonGroup[0].children[0].children;
 const onOffBtns = Array.prototype.slice.call( onOffButtons );
 
-
-
-onOffBtns.forEach((element, index, arr)=> {
-    element.addEventListener('click', ()=>
-    {console.log(arr[index]);
-    })
-})
-
-close.addEventListener('click', ()=>{
-    console.log('modal closed');
-})
-
-
-//TRYTRYTRY TO BUILD SOMETHING THAT CAN BUILD UNLIMITED STRINGS/KEYS ETC
-//the string setter is all the user can change (there would have to be an svg changery thing as well)
-
-
-//part of a method
-// let correctFretsHide = document.querySelectorAll(a);
-// let correctFretsShow = document.querySelectorAll(a);
-// let celebration = document.querySelectorAll(a)
+const ukuleleStrings = ['g', 'c', 'e', 'a'];
 
 
 
-class Fret{
-    constructor(strings, frets, noteNames, speechBubble, scoreCard, startButtons, modal, I, fretArray){
-        strings = this.strings;
-        frets = this.frets;
-        noteNames = this.noteNames;
-        speechBubble = this.speechBubble;
-        scoreCard = this.scoreCard;
-        startButtons = this.startButtons;
-        modal = this.modal;
-        I = this.I;
-        this.fretArray = [];
-    }
-    notify(noteLabels, noteNames, noteColors, noteLetters){
-        
+//double up notes for looping!
+// const notes = ["a", "a#", "b", "c", "c#", "d", "d#", "e", "f", "f#", "g", "g#", "a", "a#", "b", "a", "aSharp", "b", "c", "c#", "d", "d#", "e", "f", "f#", "g", "g#", "a", "a#", "b"];
+// const frets = function(fret){
+//     document.querySelectorAll('.frets')[fret].firstElementChild;
+// }
 
-    //THESE WILL BE ADDED WHEN WE CREATE A 'NEW' OBJECT
-        let GStringStart = 0;
-        let CStringStart = 12;
-        let EStringsStart = 25;
-        let AStringStart = 36;
-
-        // const newString = lastString + 12
-    //still vvv importand---the foreach supplies the index number!!!!!
-    //METHOD SUSSES O--- added in the 'new' object--- as will string number!
-        const G = ukulele.noteNames.indexOf('g');
-        const C = ukulele.noteNames.indexOf('c');
-        const E = ukulele.noteNames.indexOf('e');
-        const A = ukulele.noteNames.indexOf('a');
-        console.log(C);
-        this.frets=frets;
-
-        this.noteNames = ["a", "aSharp", "b", "c", "cSharp", "d", "dSharp", "e", "f", "fSharp", "g", "gsharp", "a", "aSharp", "b", "c", "cSharp", "d", "dSharp", "e", "f", "fSharp", "g", "gsharp"];
-    //variables i and len MIGHT need to be this'd or maybe just be paramatered in the method!?
-        
-
-        for (this.I = C; this.I < 12 + C; this.I++) {
-            this.fretArray.push(this.frets[CStringStart ++]);
-        }
-        // console.log(this.fretArray);
-        
-        this.fretArray.forEach(element => {console.log(element)});
-
-    }
-    // newString(a, b){
-    //     let a = new Array;
-    //     for(this.I = a; this.I < 12 + a; this.I++){
-
-    //     }
-    // }
+//turning the notes into an expression allows us to change the suffix on the sharp notes
+const notes = function(shrp){
+    return [`a`, `a${shrp}`, `b`, `c`, `c${shrp}`, `d`, `d${shrp}`, `e`, `f`, `f${shrp}`, `g`, `g${shrp}`, `a`, `a${shrp}`, `b`, `c`, `c${shrp}`, `d`, `d${shrp}`, `e`, `f`, `f${shrp}`, `g`, `g${shrp}`];
 }
-
-//number of frets, start note and number of notes! 
-const fretUI = new Fret(strings, frets);
-
-//THIS!!!! WILL MAKE THE FRET TARGETING MUCH EASIER
-//FOREACH OR FOR COMBINED WITH THE NOTE FOR LOOP---
-//WILL ALLOW US TO CREATE A NICE METHOD FOR EACH FRET
-//MAYBE DUE TO THE SOUNDS, FUNCTIONS ETC WE HAVE AN ACTUAL FRET CLASS?
+let noteColors =["ffff99", "FFCC33","FF9933", "cc3300", "333399", "666699", "336666", "66CC33", "99CC33", "CCCC33", "99CC33", "ffff66", "ffff99", "FFCC33","FF9933", "cc3300", "333399", "666699", "336666", "66CC33", "99CC33", "CCCC33", "99CC33", "ffff66"];
 
 //SELECTOR FOR BACKGROUND---- FIRST FRET
 //document.querySelectorAll('.fret')[1].firstElementChild.style.fill='blue'
@@ -96,4 +30,132 @@ const fretUI = new Fret(strings, frets);
 //document.querySelectorAll('.fret')[1].style.fill='WHITE'
 //SELECTOR FOR THE FRET TEXT---FIRST, HOWEVER ONE BACK AND WE CAN DO A FOR EACH
 
-//text styles will still need to be supplied now because innertext html class has been deleted!
+class FretBoard{
+    constructor(notes, colors, frets, fretNotes, sounds, ukuleleStrings, ntNmb, strtFrt){
+        this.notes = notes;
+        this.colors = colors;
+        this.frets = frets;
+        this.fretNotes = fretNotes;
+        this.sounds = sounds;
+        this.ukuleleStrings = ukuleleStrings;
+        this.ntNmb;
+        this.strtFrt;
+
+    }
+    notify(startFret, startNote, i){
+        //sets the letter inside the fret (this.fretNotes) to this.note, using the # symbol for the sharps
+        
+        this.startFret=startFret;
+        this.startNote=startNote;
+
+        //HIDES THE LETTERS!!!
+
+        document.querySelectorAll('.fretNote').forEach((element)=>{element.style.display='none'})
+            
+        for(i = 0; i < 12; i++){
+            startFret++;
+            // console.log(this.fretNotes[startFret].firstElementChild.innerHTML);
+            // console.log(startFret)
+            startNote++;
+            // console.log(this.notes('#')[startNote])
+            this.fretNotes[startFret].innerHTML=this.notes('#')[startNote];
+
+            this.frets[startFret].firstElementChild.style.fill=this.colors[startNote];
+            this.frets[startFret].style.fillOpacity='0';
+            // this.frets[startFret].firstElementChild.style.fill='white';
+
+
+            this.frets[startFret].classList.add(this.notes('#')[startNote]);
+            // this.frets[startFret].firstElementChild.style.fill='white';
+
+            this.frets[startFret].addEventListener('click', ()=>{
+//EVENT BUBBLING? EVENT BUBBLING?  EVENT BUBBLING?  EVENT BUBBLING?     
+                // event.target.style.fillOpacity='0.5';
+                // event.target.style.animation='clicked 0.5s forwards'
+                // event.target.classList.add('clickedIt');
+            });
+
+            this.frets[startFret].firstElementChild.addEventListener('mouseover', ()=>{
+                // console.log(event.target);
+                // event.target.style.fillOpacity='0.5';
+                
+                // event.target.style.animation='hoverOn 0.5s forwards'
+
+
+            });
+            this.frets[startFret].firstElementChild.addEventListener('mouseleave', ()=>{
+                // console.log(event.target);
+                // event.target.style.animation='hoverOff 0.5s forwards'
+            });
+            this.frets[startFret].firstElementChild.addEventListener('touchmove', ()=>{
+                // console.log(event.target);
+                // touches.style.animation='hoverOn 0.5s forwards';
+                // this.frets[startFret].firstElementChild.style.animation='hoverOn 0.5s forwards';
+             
+                
+            });
+
+
+
+        }
+    }
+    fretify(stringNote, startFret, startNote){
+        startFret = -12;
+        document.querySelector('.drawing').addEventListener('touchstart', (ev)=>{
+            // console.log(ev);
+            if(event.target.tagName==='path'){
+                // console.log(event.touches);
+                // event.target.style.animation='hoverOn 1s'
+            }
+        });
+        document.querySelector('.drawing').addEventListener('touchend', (ev)=>{
+            // console.log(ev.touches);
+            // console.log(ev.type);
+            if(event.target.tagName==='path'){
+                // console.log(event.touches);
+                // event.target.style.animation='hoverOff 1s'
+            }
+        });
+        //CREATE AND DISPATCH CUSTOM EVENTS!
+        document.querySelector('.drawing').addEventListener('touchmove', (ev)=>{
+            console.log('x =' + event.touches[0].clientX + ' y =' + event.touches[0].clientY);
+            // console.log(event.touches[0].clientY);
+
+            if(event.target.tagName==='path'){
+                // console.log(event.touches); 
+                event.target.style.animation='hoverOff 1s'
+            }
+        });
+        // document.querySelector('.drawing').addEventListener('touchmove', f)
+        //     // console.log(event.target.parentElement);
+        //     function f(ev){
+        //         console.log(ev)
+        //         if(event.target.tagName==='path'){
+        //             console.log(event.touches);
+        //             event.target.style.animation='hoverOn 1s'
+        //         }
+        //     }
+            
+
+        
+
+        stringNote.forEach((element, index)=>{
+            startFret = 12 * index;
+            startNote = notes('#').indexOf(element);
+            // console.log(element, startFret, stringNote);
+            // console.log(startFret);
+            // console.log(startNote);
+            // this.notify(startNote, startFret);
+           this.notify(startFret, startNote);
+        })
+    }
+}
+
+
+const ukuleleBoard = new FretBoard(notes, noteColors, frets, fretNotes, 'sound');
+
+ukuleleBoard.fretify(ukuleleStrings);
+
+
+        //CREATE AND DISPATCH CUSTOM EVENTS!
+        //(https://www.youtube.com/watch?v=k5TSidZEH5s&list=PLyuRouwmQCjmQTKvgqIgah03HF1wrYkA9&index=44)
---
+++ menu.js
@@ -0,0 +1,100 @@
+// onOffBtns.forEach((element, index, arr)=> {
+//     element.addEventListener('click', ()=>
+//     // {console.log(arr[index]);
+//     })
+// })
+
+close.addEventListener('click', ()=>{
+    // console.log('modal closed');
+})
+
+
+
+
+
+//part of a method
+// let correctFretsHide = document.querySelectorAll(a);
+// let correctFretsShow = document.querySelectorAll(a);
+// let celebration = document.querySelectorAll(a)
+
+
+//SELECTORS, EVENTS LISTENERS, STYLING AND ADDING ATTRIBUTES (SOUNDS/ COLORS) ALL DONE INSIDE THE CLASS
+//SO IS ATTACHING EACH NEW CLASS WITH ITS BITS TO EACH RELEVANT FRET!!!
+
+
+
+
+class Fret{
+    constructor(strings, frets, noteNames, speechBubble, scoreCard, startButtons, modal, I, fretArray){
+        strings = this.strings;
+        frets = this.frets;
+        noteNames = this.noteNames;
+        speechBubble = this.speechBubble;
+        scoreCard = this.scoreCard;
+        startButtons = this.startButtons;
+        modal = this.modal;
+        I = this.I;
+        this.fretArray = [];
+    }
+    notify(noteLabels, noteNames, noteColors, noteLetters){
+        
+
+    //THESE WILL BE ADDED WHEN WE CREATE A 'NEW' OBJECT
+        let GStringStart = 0;
+        let CStringStart = 12;
+        let EStringsStart = 25;
+        let AStringStart = 36;
+
+        // const newString = lastString + 12
+    //still vvv importand---the foreach supplies the index number!!!!!
+    //METHOD SUSSES O--- added in the 'new' object--- as will string number!
+        const G = ukulele.noteNames.indexOf('g');
+        const C = ukulele.noteNames.indexOf('c');
+        const E = ukulele.noteNames.indexOf('e');
+        const A = ukulele.noteNames.indexOf('a');
+        this.frets=frets;
+
+        this.noteNames = ["a", "aSharp", "b", "c", "cSharp", "d", "dSharp", "e", "f", "fSharp", "g", "gsharp", "a", "aSharp", "b", "c", "cSharp", "d", "dSharp", "e", "f", "fSharp", "g", "gsharp"];
+    //variables i and len MIGHT need to be this'd or maybe just be paramatered in the method!?
+        
+
+        for (this.I = C; this.I < 12 + C; this.I++) {
+            this.fretArray.push(this.frets[CStringStart ++]);
+        }
+        // console.log(this.fretArray);
+        
+        // this.fretArray.forEach(element => {console.log(element)});
+
+    }
+    // newString(a, b){
+    //     let a = new Array;
+    //     for(this.I = a; this.I < 12 + a; this.I++){
+
+    //     }
+    // }
+}
+//selectt frot by class
+//document.querySelectorAll('.fretNoteLetter')[1].setAttribute('font-size', '1em')
+
+//number of frets, start note and number of notes! 
+const fretUI = new Fret(strings, frets);
+
+//THIS!!!! WILL MAKE THE FRET TARGETING MUCH EASIER
+//FOREACH OR FOR COMBINED WITH THE NOTE FOR LOOP---
+//WILL ALLOW US TO CREATE A NICE METHOD FOR EACH FRET
+//MAYBE DUE TO THE SOUNDS, FUNCTIONS ETC WE HAVE AN ACTUAL FRET CLASS?
+
+
+
+//fretboard leftovers
+
+// const cunt = document.querySelectorAll('.fretNote')[1].firstElementChild.innerHTML='F#';
+
+// const cuntoo = document.querySelectorAll('.fretNote')[1].innerHTML='5';
+
+// const cuntThree = document.querySelectorAll('.fret')[1].firstElementChild.style.fill='blue';
+
+
+// c#
+// d
+// d#
