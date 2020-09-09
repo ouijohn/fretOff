77f9de7e62fa3f131166ab30bb2d3921f040200d
ouijohn <johnsspaces@gmail.com>
2020-09-09 17:26:28 +0100

    touchevents ACTUALLY done (trimmed and noted)

--- fretboardUI.js
+++ fretboardUI.js
@@ -1,3 +1,4 @@
+const theWholeThing = document.querySelector('main');
 const messageBubble = document.querySelector(".message");
 const strings = document.querySelectorAll(".string");
 const frets = document.querySelectorAll('.fret');
@@ -17,85 +18,116 @@ const notes = function(shrp){
 }
 //the same note colors could be used for multiple instruments
 let noteColors =["ffff99", "FFCC33","FF9933", "cc3300", "333399", "666699", "336666", "66CC33", "99CC33", "CCCC33", "99CC33", "ffff66", "ffff99", "FFCC33","FF9933", "cc3300", "333399", "666699", "336666", "66CC33", "99CC33", "CCCC33", "99CC33", "ffff66"];
-
+//class collects and holds the text content, color and sound information for each fret and attaches the methods and events to them
 class FretBoard{
-    constructor(notes, colors, frets, fretNotes, sounds, ukuleleStrings, ntNmb, strtFrt){
+    constructor(notes, colors, frets, fretNotes, sounds, ukuleleStrings, ukeNoteKey, ntNmb, strtFrt, touchtivity){
         this.notes = notes;
         this.colors = colors;
         this.frets = frets;
         this.fretNotes = fretNotes;
         this.sounds = sounds;
         this.ukuleleStrings = ukuleleStrings;
-        this.ntNmb;
+        this.ukeNoteKey = ukeNoteKey;
         this.strtFrt;
+        this.touchtivity = touchtivity;
     }
-    //change method to fretSound!
-    fretColor(){
-        if(event === ''){
-
+    //here is where we react to the events relating to the frets color (and trigger the sounds)
+    fretColor(element){
+    //these react to events set in the notify loop with an event listener atached to each fret
+        if(event.type === 'mouseover'){
+            event.target.style.animation='hoverOn 0.75s forwards'
+            let activeNote = event.target.parentElement.getAttribute('data-fret-sound');
+            this.soundOn(activeNote);
         }
-        if(event === ''){
-
+        if(event.type === 'mouseleave'){
+            event.target.style.animation='hoverOff 1s forwards'
         }
-        if(event === ''){
-
+        if(event.type === 'click'){
+            event.target.style.animation='noteDown 0.5s forwards'
+            ukulele.takeSelected(event.target.parentElement);
+            console.log(event.target.parentElement);
         }
-        if(event === ''){
-
+    //the touch event is set just bellow the notify loop
+        if(event.type === 'touchmove'){
+    //takes the coords of every fret touched and animates it
+            this.touchcolor(event.touches[0].clientX, event.touches[0].clientY);
+            this.touchSoundOn(event.touches[0].clientX, event.touches[0].clientY);
         }
     }
+    //maybe build a touch event to ensure the initial touch is sounded
 
+    //takes the coords, builds a var out of each touched and animates it
     touchcolor(x, y){
+        //stops the fu**ing bubbling!
         event.stopPropagation();
         let elem = document.elementFromPoint(x, y);
         if(elem.parentElement.classList.contains('fret')){
             elem.style.animation='touchOver 0.5s';
             setTimeout(()=>{
-                // elem.style.animation='hoverOff 0.5s';
                 elem.style.animation='';
-
             }, 600)
+            let activeNote = elem.parentElement.getAttribute('data-fret-sound');
+            
+            this.touchtivity.push(activeNote);
 
-            // setTimeout(()=>{
-            //     elem.parentElement.style.animation='';
-            //     elem.parentElement.style.fillOpacity='0';
-            //     console.log('cunt');
-            // }, 600)
+        //gets the atts of each note- hold onto for now
+                // console.log(activeNote);
+                // this.touchtivity.push(activeNote);
+
+                // let activeNote = elem.parentElement.getAttribute('data-fret-sound');
+                // console.log(activeNote);
+                // console.log(notes('sharp').indexOf(activeNote));
         }
     }
-    // mouseColor(action, element){
-    //     if(action === 'mouseover'){
-    //         element.style.animation='hoverOn 0.75s forwards';
-    //         setTimeout(()=>{
-    //             elem.parentElement.style.animation='';
-    //         }, 600);
-    //     }
-    //     if(action === 'mouseleave'){
-    //         element.style.animation='hoverOff 0.75s forwards';
-    //         setTimeout(()=>{
-    //             elem.parentElement.style.animation='';
-    //         }, 600);
-    //     }
-    //     if(action === 'click'){
-    //         element.style.animation='noteDown 0.75s forwards';
-    //         setTimeout(()=>{
-    //             elem.parentElement.style.animation='';
-    //         }, 600);
-    //     }
-    // }
-    // fretSound(ooo){
-    //     switch(ooo){
-    //         case 'coo':
-    //             alert('coo');
-    //         break;
-    //         case 'oo':
-    //             alert('oo');
-    //         break;
-    //         case 'o':
-    //             alert('o');
-    //         break;
-    //     }
-    // }
+
+//START USING SWITCH STATEMENTS!
+
+
+//THIS WILL REPEAT EVERY TIME YOUR FINGER MOVES!
+
+///there an object/ instantiation/ array for each letter? if so just use number one/0! Then delete the thing!!!
+
+//add each instanation of the object into one, use the first one, then delete
+
+//maybe we have a container for each note that we clear after use?
+
+//or maybe a modular one that is made, used (via the first/ 0) to sound the note then cleared
+       soundOn(note){
+            const fretSounds = document.querySelector('#fretSounds')
+            var x = document.createElement("AUDIO");
+            x.setAttribute("src","sounds/" + note + ".wav");
+            fretSounds.appendChild(x).play();
+            x.volume=0.1;
+            setTimeout(()=>{
+                x.remove()
+            }, 750)
+        }
+       touchSoundOn(x, y){
+        //we feed the coords of the element targeted by the touchmove
+        let elem = document.elementFromPoint(x, y);
+        //data-fret-sound has the title of the relevant sounds inside - aSharp, etc
+        let touchNote = elem.parentElement.getAttribute('data-fret-sound');
+        console.log(touchNote);
+        //if the playing element contains the class all we do is remove it (after it has a chance to play)
+        //this ensures we don't try to play the same note multiple times
+        if(elem.parentElement.classList.contains('playing')){
+            setTimeout(()=>{
+            elem.parentElement.classList.remove('playing')
+        }, 5000)
+        //play the note once/ trigger soundOn(coords, coords) then add the class to 
+        }else{
+            console.log('play, then add')
+            this.soundOn(touchNote);
+            elem.parentElement.classList.add('playing')
+        }
+        //an initial attempt to do the above- adding played note to array once (might be worth keeping)
+            // this.touchtivity.push('hello');
+            // console.log(this.touchtivity);
+            // this.touchtivity.push(event.target.getAttribute('id'));
+            // console.log(this.touchtivity);
+        }
+
+//builds notes and note events then adds them to the frets!
     notify(startFret, startNote, i){
     //HIDES THE LETTERS in each fret!!!
         document.querySelectorAll('.fretNote').forEach((element)=>{element.style.display='none'})
@@ -104,67 +136,69 @@ class FretBoard{
         this.startNote=startNote;
     //This does the same fot the first fret on each string 
         this.startFret=startFret;
+        
+       
     //loop targets each fret and adds the corosponding notes, colors and events
             for(i = 0; i < 12; i++){
     //incriments the note and fret numbers in oder for us to target each fret on the string
             startFret++;
-            startNote++;
-
+            startNote++; 
+            
     //should we make variables for the target elements?
-        
+    //these variables change with every instanation making the code simpler and easier to read
+            let targetFret = this.frets[startFret];
+            let fretBackGround = this.frets[startFret].firstElementChild;
+            let fretNoteText = this.fretNotes[startFret];
+
+            
     //targets the text path inside each fret and adds relevant note by calling the notes expression and adding the # symbol
-            this.fretNotes[startFret].innerHTML=this.notes('#')[startNote];
+            fretNoteText.innerHTML=this.notes('#')[startNote];
     //targets the shape/path inside the fret and adds the color/ shade using the relevant note letter/ number
-            this.frets[startFret].firstElementChild.style.fill=this.colors[startNote];
+            fretBackGround.style.fill=this.colors[startNote];
     //turns the shape/path's opacity down to 0 so that they can appear/ disapear on touch, hover, etc
-            this.frets[startFret].style.fillOpacity='0';
+             targetFret.style.fillOpacity='0';
     //adds the relevant note number into the classlist (we could set the note colors in the css using this)
-            this.frets[startFret].classList.add(this.notes('#')[startNote]);
-    //       
-            this.frets[startFret].id=(this.notes('#')[startNote]);
+            targetFret.classList.add(this.notes('#')[startNote]);
+    //setting frets id with the relevant note gives us access to it's title quickly and easilly       
+            targetFret.id=(this.notes('#')[startNote]);
+    //takes the relevant note and it's key and sets the sound
+            targetFret.dataset.fretSound=this.notes('Sharp')[startNote];
 
-
-            this.frets[startFret].addEventListener('click', ()=>{
-                event.target.style.animation='noteDown 0.5s forwards'
-
-                // this.mouseColor(event.target, 'noteDown')
-                //add check method---which triggers right or wrong/  colored/ not
+            targetFret.addEventListener('click', ()=>{
+                this.fretColor(event.target);
             });
-            this.frets[startFret].firstElementChild.addEventListener('mouseover', ()=>{
-                event.target.style.animation='hoverOn 0.75s forwards'
+            fretBackGround.addEventListener('mouseover', ()=>{
+                this.fretColor(event.target);
             });
-            this.frets[startFret].firstElementChild.addEventListener('mouseleave', ()=>{
-                setTimeout(()=>{
-
-                }, 800)
-                event.target.style.animation='hoverOff 1s forwards'
+            fretBackGround.addEventListener('mouseleave', ()=>{
+                this.fretColor(event.target);
             });
         }
-    } 
-    
+    //attach a listener to the whole page then trigger an event to each finger drag
+        theWholeThing.addEventListener('touchmove', (ev)=>{
+            this.fretColor(event.target);
 
-
-    fretify(stringNote, startFret, startNote){
-        startFret = -12;
-        
-
-        //CREATE AND DISPATCH CUSTOM EVENTS!
-        document.querySelector('main').addEventListener('touchmove', (ev)=>{
-            this.touchcolor(event.touches[0].clientX, event.touches[0].clientY)
         });
+    } 
+//triggers all the building etc
+    fretify(stringNote, startFret, startNote){
+    //CREATE AND DISPATCH CUSTOM EVENTS- both the values have to be reduced by 1 to take the index to 0!
         stringNote.forEach((element, index)=>{
-            startFret = 12 * index;
+            startFret = 12 * index - 1;
             startNote = notes('#').indexOf(element);
-            this.notify(startFret, startNote);
+            this.notify(startFret, startNote - 1);
         })
     }
 }
 
-
-const ukuleleBoard = new FretBoard(notes, noteColors, frets, fretNotes, 'sound');
+const ukuleleBoard = new FretBoard(notes, noteColors, frets, fretNotes, 'sound', 'uke', 'uke', 'uke','uke', []);
 
 ukuleleBoard.fretify(ukuleleStrings);
 
 
-        //CREATE AND DISPATCH CUSTOM EVENTS!
-        //(https://www.youtube.com/watch?v=k5TSidZEH5s&list=PLyuRouwmQCjmQTKvgqIgah03HF1wrYkA9&index=44)
+
+//if(letter/ key hass class added sound it, then move to next letter?!)
+
+
+//CREATE AND DISPATCH CUSTOM EVENTS!
+//(https://www.youtube.com/watch?v=k5TSidZEH5s&list=PLyuRouwmQCjmQTKvgqIgah03HF1wrYkA9&index=44)
--- gameBits.js
+++ gameBits.js
@@ -1,10 +1,43 @@
 class GameBits{
-    constructor(fretcounter, rightWrong, scoreCounter, bubbleMsg, notes){
-        fretcounter = this.fretcounter;
-        notes = this.notes;
-        this.noteNames = ["a", "aSharp", "b", "c", "cSharp", "d", "dSharp", "e", "f", "fSharp", "g", "gsharp", "a", "aSharp", "b", "c", "cSharp", "d", "dSharp", "e", "f", "fSharp", "g", "gsharp"];
-
-    }
+    constructor(randomNote, fretcounter, rightWrong, scoreCounter, bubbleMsg, notes){
+        this.question =this.randomGen();
+        this.fretcounter = fretcounter;
+        this.notes = notes;
+        this.rightWrong = rightWrong;
+        this.scoreCounter = scoreCounter;
+        this.bubbleMsg = bubbleMsg;
         
+    }
+    
+    randomGen(){
+        let rndmNoteNumber = Math.floor(Math.random() * (notes('#').length));
+        let scoreArray = notes('#');
+        // console.log(scoreArray[rndmNoteNumber]);
+        return(scoreArray[rndmNoteNumber]);
+    }
+    takeSelected(a){
+        this.compare(a.getAttribute('id'));
+    }
+    compare(answer){
+        console.log(this.question);
+        console.log(answer);
+        if(answer === this.question){
+            // alert('hurray')
+        }else{
+            // alert('boooo');
+        }
+        
+    }
+    // result(){
+
+    // }
+    // onOffButton(){
+
+    // }
+    // randomFrets(){
+
+    // }
+
 }
-const ukulele = new GameBits();
+const ukulele = new GameBits('uke' ,'uke','uke','uke','uke','uke');
+ukulele.randomGen();
